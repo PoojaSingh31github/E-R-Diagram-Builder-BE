c207b6fe3c24f25a39f38c508050c443e018529b
PoojaSingh31github <pooja.singh.masaischool@gmail.com>
2024-12-21 12:12:46 +0530

    CORS policy

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ const PORT = process.env.PORT || 8080;
 
 connectDB();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: ["http://localhost:5173", "*"],
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/project", projectRoute)
