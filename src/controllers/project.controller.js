eb64526ea10486c010a4405a871591786e1b4138
PoojaSingh31github <pooja.singh.masaischool@gmail.com>
2024-12-22 17:55:18 +0530

    update project controller and model

--- src/controllers/project.controller.js
+++ src/controllers/project.controller.js
@@ -1,9 +1,7 @@
 import Project from "../models/project.modal.js";
 
-
 export const createProject = async (req, res) => {
-    const { name, description, nodes, edges , userId, type } = req.body;
-    console.log(res.body)
+    const { name, description, nodes, edges, userId, type, tables } = req.body;
 
     try {
         const project = new Project({
@@ -13,6 +11,7 @@ export const createProject = async (req, res) => {
             nodes,
             edges,
             type,
+            tables: tables || [], 
         });
 
         await project.save();
@@ -32,13 +31,14 @@ export const getAllProjects = async (req, res) => {
     }
 };
 
+
 export const getProjectById = async (req, res) => {
     try {
         const project = await Project.findById(req.params.projectId);
         if (!project) {
             return res.status(404).json({ message: "Project not found" });
         }
-        res.status(200).json(project);
+        res.status(200).json(project);  
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch project", error });
     }
@@ -46,12 +46,12 @@ export const getProjectById = async (req, res) => {
 
 
 export const updateProject = async (req, res) => {
-    const { name, description, nodes, edges } = req.body;
+    const { name, description, nodes, edges, tables } = req.body;
 
     try {
         const updatedProject = await Project.findByIdAndUpdate(
             req.params.projectId,
-            { name, description, nodes, edges },
+            { name, description, nodes, edges, tables },  
             { new: true }
         );
 
@@ -63,7 +63,6 @@ export const updateProject = async (req, res) => {
         res.status(500).json({ message: "Failed to update project", error });
     }
 };
-
 
 export const deleteProject = async (req, res) => {
     try {
--- src/models/project.modal.js
+++ src/models/project.modal.js
@@ -6,8 +6,19 @@ const projectSchema = new mongoose.Schema({
     type: { type: String, required: true, enum: ["erd", "sd"], default: "erd" },
     owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     nodes: { type: mongoose.Schema.Types.Mixed, default: [] }, 
-    edges: { type: mongoose.Schema.Types.Mixed, default: [] }, 
-
+    edges: { type: mongoose.Schema.Types.Mixed, default: [] },
+    tables: [
+        {
+            name: { type: String, required: true },
+            columns: [
+                {
+                    name: { type: String, required: true },
+                    type: { type: String, required: true },
+                }
+            ],
+            comments: [String],  
+        }
+    ],
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 }, { timestamps: true });
